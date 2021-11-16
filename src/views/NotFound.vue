692f92918fdeb4d26da568276d18700c7731debc
oudaxiong <770869584@qq.com>
2021-11-16 22:45:24 +0800

    添加404页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+  <div>当前页面访问不存在</div>
+  <div>
+    <router-link to="/">返回首页</router-link>
+  </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'NotFound'
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
