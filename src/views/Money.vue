2bfa62c59609ad49b5d51227f525c626884484a4
oudaxiong <770869584@qq.com>
2022-03-15 22:25:36 +0800

    将create 封装到 recordListModel.ts

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -55,14 +55,12 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    const record2: RecordItem = recordListModel.clone(this.record);
-    record2.createdAt = new Date();
-    this.recordList.push(record2);
+    recordListModel.create(this.record);
   }
 
   @Watch('recordList')
   onRecordListChange() {
-    recordListModel.save()
+    recordListModel.save();
   }
 };
 </script>
@@ -72,7 +70,8 @@ export default class Money extends Vue {
   display: flex;
   flex-direction: column-reverse;
 }
-.notes{
+
+.notes {
   padding: 12px 0;
 }
 </style>
