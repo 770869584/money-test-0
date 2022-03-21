c97491e318aeeaa5ced16ed0efaa1f5d0e21feb1
oudaxiong <770869584@qq.com>
2022-03-21 22:40:23 +0800

    完成列表的css

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,11 +5,7 @@ import createId from '@/lib/idCreator';
 
 Vue.use(Vuex);
 
-type RootState = {
-  recordList: RecordItem[],
-  tagList: Tag[],
-  currentTag?: Tag
-}
+
 
 const store = new Vuex.Store({
   state: {
@@ -23,7 +19,7 @@ const store = new Vuex.Store({
     },
     createRecord(state, record) {
       const record2: RecordItem = clone(record);
-      record2.createdAt = new Date();
+      record2.createdAt = new Date().toISOString();
       state.recordList.push(record2);
       store.commit('saveRecords');
     },
