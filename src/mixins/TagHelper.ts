1a353d3e36aa6e8449211dc968094891c04524e6
oudaxiong <770869584@qq.com>
2022-03-31 21:18:03 +0800

    修复好多bug

--- src/mixins/TagHelper.ts
+++ src/mixins/TagHelper.ts
@@ -1,5 +1,10 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
+
+const map : {[key:string]:string} = {
+  'tag name duplicated':'标签名重复了'
+}
+
 @Component
 export class TagHelper extends Vue{
   createTag() {
@@ -8,6 +13,11 @@ export class TagHelper extends Vue{
       return window.alert('标签名不能为空');
     }
     this.$store.commit('createTag',name);
+    if (this.$store.state.createTagError){
+      window.alert(map[this.$store.state.createTagErro.message] || '未知错误')
+    } else {
+      window.alert('已保存')
+    }
   }
 }
 
