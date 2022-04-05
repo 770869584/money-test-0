bf9659f977293d688cfdcdd7f81eb71f04b888c0
oudaxiong <770869584@qq.com>
2022-04-05 23:42:57 +0800

    回归一次

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,9 @@ const path = require('path');
 
 
 module.exports = {
-  publicPath: '',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/morney-3-website/'
+    : '/',
   lintOnSave: false,
   chainWebpack: config => {
     const dir = path.resolve(__dirname, 'src/assets/icons');
